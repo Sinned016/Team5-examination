0e177e11badb2780c09a3dd3d7c9f7853a5ab91e
Dennis Blomberg <97sinned@gmail.com>
2023-10-11 14:42:32 +0200

    Added a GET to fetch a single movie with ObjectId that you get from params

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { ObjectId } from "mongodb";
 import { fetchCollection } from "../mongo/mongoClient.js";
 
 const router = express.Router();
@@ -14,5 +15,22 @@ router.get("/movies", async (req, res) => {
     }
 })
 
+// GladiatorId = 65157cedcc65c28c5afd3ef9
+router.get("/movie/:id", async (req, res) => {
+    const movieId = req.params.id
+
+    if(ObjectId.isValid(movieId)) {
+
+        const movie = await fetchCollection("movies").findOne({_id: new ObjectId(movieId)})
+
+        if(movie == null) {
+            res.status(404).send({error: "Could not fetch the document"})
+        } else {
+            res.status(200).send(movie)
+        }
+    } else {
+        res.status(404).send({error: "ObjectId is not valid"})
+    }
+})
 
 export default router;
