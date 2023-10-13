d12f07b3859876d04d5efd493aa5313e04ab4c1a
Sandra J <113009763+Sajo0804@users.noreply.github.com>
2023-10-13 13:37:42 +0200

    Update screeningRouter.js
    GET fetch all screenings.

--- backend/src/router/screeningRouter.js
+++ backend/src/router/screeningRouter.js
@@ -1,1 +1,18 @@
 
+import express from "express";
+import { fetchCollection } from "../mongo/mongoClient.js";
+
+const router = express.Router();
+
+//GET screenings
+router.get("/screenings", async (req, res) => {
+  try {
+    const screenings = await fetchCollection("screenings").find().toArray();
+    res.send(screenings);
+  } catch (err) {
+    res.status(500);
+    res.send(err.clientMessage);
+  }
+});
+
+export default router;
