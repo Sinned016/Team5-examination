f6b5610765757eda0fc52c2ad669644db0f4555e
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-13 11:19:01 +0200

    added comments in authController file

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { fetchCollection } from "../mongo/mongoClient.js";
-
+// create/insert a new user document in MongoDB, including email, hashed password and role.
 async function createUser(email, password) {
   const salt = await bcrypt.genSalt();
   const hashedPassword = await bcrypt.hash(password, salt);
@@ -10,17 +10,19 @@ async function createUser(email, password) {
 
   return fetchCollection("users").updateOne(filter, update, options);
 }
+
+// handle post request and validate the input
 async function register(req, res) {
   const { email, password } = req.body;
   if (email == undefined || password == undefined)
-    return res.status(400).send("Missing email or password");
+    return res.status(400).send("Missing email or password"); // 400: bad request
 
   let result = await createUser(email, password);
 
   if (result.upsertedCount == 1) {
-    return res.status(201).send("Account created successfully!");
+    return res.status(201).send("Account created successfully!"); // 201: created
   } else {
-    return res.status(409).send("Account already exists"); // 409: Conflict
+    return res.status(409).send("Account already exists"); // 409: conflict
   }
 }
 
