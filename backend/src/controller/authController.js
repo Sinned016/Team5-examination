6054dd7b5f8cf64a13ca8e109256bde43cc4c278
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-16 10:26:47 +0200

    added login and jwt

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,5 +1,9 @@
 import bcrypt from "bcrypt";
 import { fetchCollection } from "../mongo/mongoClient.js";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = "iwillnottellyoufsfsfsfssfwrwrjwrojjhfsjfhsfj";
+
 // create a new user with email, hashed password and role if the email is not existing.
 async function createUser(email, password) {
   const salt = await bcrypt.genSalt();
@@ -26,4 +30,26 @@ async function register(req, res) {
   }
 }
 
-export default { register };
+// Handle post request for logging in
+// Authenticate when user login
+async function authenticate(email, password) {
+  const result = await fetchCollection("users").findOne({ email });
+  if (result == undefined) return false;
+  const isMatch = await bcrypt.compare(password, result.password);
+  return isMatch;
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  if (email == undefined || password == undefined) return res.sendStatus(400);
+
+  const isMatch = await authenticate(email, password);
+
+  if (isMatch) {
+    const accessToken = jwt.sign({ email }, JWT_SECRET);
+    return res.status(200).send({ accessToken });
+  } else {
+    return res.status(400).send("Bad credentials");
+  }
+}
+export default { register, login };
