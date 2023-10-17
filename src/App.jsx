2adeb4a6334c33a2636162c68047abcbcaf0006d
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-17 12:58:31 +0200

    setup socket.io

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { Outlet } from 'react-router-dom';
-
+import { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
+  const [socket, setSocket] = useState(null);
 
-  return <>
-    <header>
-      <h1>Header component here</h1>
-    </header>
-    <main>
-      <Outlet />
-    </main>
-    <footer className='container-fluid mt-4'>
-      <h3>Footer component here</h3>
-    </footer>
-  </>
+  // Setup the socket
+  useEffect(() => {
+    const newSocket = socketIOClient("http://localhost:5003");
+    setSocket(newSocket);
+
+    // Clean up: Disconnect the socket as the component unmounts
+    return () => newSocket.disconnect();
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1>Header component here</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer className="container-fluid mt-4">
+        <h3>Footer component here</h3>
+      </footer>
+    </>
+  );
 }
