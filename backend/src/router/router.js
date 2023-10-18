763eaa92d6347a8d2da829d4c00700ed2b9e33ff
Dennis Blomberg <97sinned@gmail.com>
2023-10-18 13:20:00 +0200

    res.send is now returning which seats the user booked and the full price

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -119,11 +119,7 @@ router.put("/screening/:id", async (req, res) => {
   ) {
     return res.status(400).send("Missing information"); // 400: bad request
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 2bb5ffbbeea79a2dbb69c013b272e90eabacda2c
   if (ObjectId.isValid(screeningId)) {
     const existingBooking = await fetchCollection("bookings").findOne({
       bookingNumber: bookingNumber
@@ -141,7 +137,10 @@ router.put("/screening/:id", async (req, res) => {
 
         const insertedId = bookedResult.insertedId;
 
-        let results = []
+        let results = {
+          fullprice: fullPrice,
+          bookedSeats: []
+        }
         for (let i = 0; i < bookingInformation.bookedSeats.length; i++) {
           const bookedSeatsString = `seats.${bookingInformation.bookedSeats[i][0]}.${bookingInformation.bookedSeats[i][1]}`;
           const result = await fetchCollection("screenings").updateOne(
@@ -149,9 +148,9 @@ router.put("/screening/:id", async (req, res) => {
             { $set: { [bookedSeatsString]: insertedId } });
 
         if (result.modifiedCount === 1) {
-          results.push({result: `You booked seat ${bookingInformation.bookedSeats[i]}`, price: fullPrice});
+          results.bookedSeats.push({result: `You booked seat ${bookingInformation.bookedSeats[i]}`});
         } else {
-          results.push({error: `Seat ${bookingInformation.bookedSeats[i]} is already taken`});
+          results.bookedSeats.push({error: `Seat ${bookingInformation.bookedSeats[i]} is already taken`});
         }
       }
       res.send(results);
