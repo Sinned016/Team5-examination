5f569bbc8df604b5e9a2109eb4c3b72e9fb8708a
Dennis Blomberg <97sinned@gmail.com>
2023-10-23 15:19:23 +0200

    added all necessary folders so we dont create duplicates

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ export default function App() {
   return (
     <>
       <header>
-        <h1>Header component here</h1>
+        <h1>Header component here!</h1>
       </header>
       <main>
         <Outlet />
       </main>
       <footer className="container-fluid mt-4">
-        <h3>Footer component here</h3>
+        <h3>Footer component here!</h3>
       </footer>
     </>
   );
