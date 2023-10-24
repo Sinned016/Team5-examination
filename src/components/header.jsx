f7906bd99551d7dfa612abff61ef5b7655dc2e77
sajo0804 <sandrajonsson86@live.se>
2023-10-24 10:08:27 +0200

    small changes in dropdown and installed bootstrap-icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,22 +8,26 @@ export default function AppHeader() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
+        {/* Ändra target i href */}
         <Navbar.Brand href="#home">Filmvisarna</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            {/* <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link> */}
+            <NavDropdown title="SJ" id="basic-nav-dropdown">
+              {/* Ändra target i href */}
+              <NavDropdown.Item href="#register">
+                Bli medlem
+                <img
+                  src="/public/images/profillogo.png"
+                  width={25}
+                  height={25}
+                  alt="Profile logo"
+                />
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              {/* Ändra target i href */}
+              <NavDropdown.Item href="#login">Logga in</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
