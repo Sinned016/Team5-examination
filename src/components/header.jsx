72e523f59e056fc152ed4526ef5b80a64b910b78
Danblad <rogerdanblad@gmail.com>
2023-10-25 15:55:20 +0200

    Image now changes size responsively and the dropdownmenu is mmoved inwards instead of downwards

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,39 +5,37 @@ import "../sass/header.scss";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
-// import Nav from "react-bootstrap/Navbar";
-
 export default function Header() {
   return (
     <Navbar expand="lg" className="navbar-body">
       <Container>
-        {/* Kolla target i href */}
-        <Navbar.Brand href="#/screeningsAndMovies">
-        <img
+      <div className="logo-container">
+        <a href="/">
+          <img
             className="company-logo"
             src="/public/images/filmvisarna_logotyp.png"
-            width="auto"
-            height="110"
+            alt="Company Logo"
           />
-        </Navbar.Brand>
-
+        </a>
+      </div>
         <DropdownButton
-          id="dropdown-basic-button"
-          title={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              className="bi bi-person-circle"
-              viewBox="0 0 16 16">
+            id="dropdown-basic-button"
+            title={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-person-circle"
+                viewBox="0 0 16 16">
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
               <path
                 fillRule="evenodd"
                 d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
               />
             </svg>
-          }>
+          } className="dropdown-button"
+            drop="start">
           <Dropdown.Item href="#/register">
             Bli medlem {""}{" "}
             <svg
@@ -70,8 +68,7 @@ export default function Header() {
               />
             </svg>
           </Dropdown.Item>
-
-          {/* När inloggad! */}
+            {/* När inloggad! */}
           {/* <Dropdown.Item href="#/bookings/:email">
             Bokningar {""}{" "}
             <svg
