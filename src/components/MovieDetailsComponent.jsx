0f824990516c2a6e357dc4ebd642bb77499e9259
Mikael Forstén <m4jken@gmail.com>
2023-10-25 15:28:28 +0200

    put down html and basic css

--- src/components/MovieDetailsComponent.jsx
+++ src/components/MovieDetailsComponent.jsx
@@ -1,17 +1,52 @@
 import { useParams } from "react-router-dom";
 import { useStates, useFetch } from "react-easier"
+import "../sass/variables.scss";
+import "../sass/movieDetailsComponent.scss";
 
 export default function MovieCard() {
     
     const routeParams = useParams();
     const id = routeParams.id;
 
-    const movieDetails = useStates({
+    const movieFetch = useStates({
         movieDetails: useFetch(`/api/movie/${id}`)
     })
     
+    const movieDetails = movieFetch.movieDetails[0];
+    const movieTrailerLink = `https://www.youtube.com/embed/${movieDetails.link}`;
+
     return (
-            <div>
+            <div className="movie-details-component">
+                <div className="movie-details-trailer">
+                    <iframe src={movieTrailerLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div><hr />
+                <div className="movie-details-box">
+                    <div className="movie-details-age-rest">
+                        <p class="text-primary-detail-color">Åldersgräns</p>
+                        <p>{movieDetails.ageRestriction}</p>
+                    </div>
+                    <div className="movie-details-subs">
+                        <p class="text-primary-detail-color">Undertexter</p>
+                        <p>{movieDetails.subtitles}</p>
+                    </div>
+                    <div className="movie-details-lang">
+                        <p class="text-primary-detail-color">Språk</p>
+                        <p>{movieDetails.language}</p>
+                    </div>
+                    <div className="movie-details-runtime">
+                        <p class="text-primary-detail-color">Längd</p>
+                        <p>{movieDetails.runtimeMin}</p>
+                    </div>
+                </div><hr />
+                <div className="movie-details-desc">
+                    {movieDetails.description}
+                </div><hr />
+                <div className="movie-details-actors">
+                    <p><span className="text-secondary-detail-color">Skådespelare:</span> {movieDetails.actors.join(", ")}</p>
+                </div>
+                <div className="movie-details-director">
+                    <p><span className="text-secondary-detail-color">Regisör:</span> {movieDetails.director}</p>
+                </div>
             </div>
     );
 }
