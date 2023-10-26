363028d05d2555db905d9cb71f823794632d3a2b
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-26 13:32:50 +0200

    loginPage connected with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
-import MovieBookingComponent from "./components/MovieBookingComponent";
-import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
 
 export default function App() {
@@ -11,15 +9,11 @@ export default function App() {
         <Header />
       </header>
 
-      <LoginPage />
-
       <main>
         <Container>
           <Outlet />
         </Container>
       </main>
-
-      <MovieBookingComponent />
 
       <footer className="container-fluid mt-4">
         <h3>Footer component here!</h3>
