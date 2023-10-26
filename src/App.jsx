97c38e0b786b7d3bcfa1cfe5f9ff7fe47bc09504
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-26 11:22:11 +0200

    loginPage merged with updated dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import MovieBookingComponent from "./components/MovieBookingComponent";
 import LoginPage from "./pages/LoginPage";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 export default function App() {
   return (
