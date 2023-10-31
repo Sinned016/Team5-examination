02950cf926b46b378ae863f11954a32b9b044e3a
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-10-31 09:32:46 +0100

    fixed css for pop up

--- src/components/MovieBookingComponent.jsx
+++ src/components/MovieBookingComponent.jsx
@@ -33,7 +33,7 @@ function MovieBookingComponent() {
     setChildTickets(childTickets);
     setTotalTickets(adultTickets + seniorTickets + childTickets);
     console.log("adult:", adultTickets, "senior:", seniorTickets, "child:", childTickets);
-  
+
     if (adultTickets > 0 || seniorTickets > 0 || childTickets > 0) {
       setActiveItem(2);
     } else {
@@ -76,10 +76,12 @@ function MovieBookingComponent() {
       <Accordion activeKey={activeItem + ""}>
         <Accordion.Item eventKey="0">
           <Accordion.Header>1. Välj visning</Accordion.Header>
-          <Accordion.Body><ScreeningListComponent setActiveItem={setActiveItem} setScreening={setScreening} /></Accordion.Body>
+          <Accordion.Body>
+            <ScreeningListComponent setActiveItem={setActiveItem} setScreening={setScreening} />
+          </Accordion.Body>
         </Accordion.Item>
 
-        <Accordion.Item eventKey="1" >
+        <Accordion.Item eventKey="1">
           <Accordion.Header>2. Biljettyp och antal</Accordion.Header>
           <Accordion.Body>
             <TicketWithPriceComponent onGetTickets={getTickets} />
@@ -88,7 +90,16 @@ function MovieBookingComponent() {
 
         <Accordion.Item eventKey="2">
           <Accordion.Header>3. Välj platser</Accordion.Header>
-          <Accordion.Body> <MovieSeatsComponent setActiveItem={setActiveItem} screening={screening} totalTickets={totalTickets} chosenSeats={chosenSeats} setChosenSeats={setChosenSeats}/> </Accordion.Body>
+          <Accordion.Body>
+            {" "}
+            <MovieSeatsComponent
+              setActiveItem={setActiveItem}
+              screening={screening}
+              totalTickets={totalTickets}
+              chosenSeats={chosenSeats}
+              setChosenSeats={setChosenSeats}
+            />{" "}
+          </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="3">
@@ -120,10 +131,13 @@ function MovieBookingComponent() {
           <Modal.Title>Bokningsbekräftelse</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Är du säker på att boka {totalTickets} biljetter för filmen
-          {screening && screening.movieDetails && screening.movieDetails.length > 0
-            ? screening.movieDetails[0].title
-            : " "}
+          Är du säker på att boka <span style={{ fontWeight: "bold" }}>{totalTickets} </span>
+          biljetter för filmen{" "}
+          <span style={{ fontWeight: "bold" }}>
+            {screening && screening.movieDetails && screening.movieDetails.length > 0
+              ? screening.movieDetails[0].title
+              : "ingen filmtitle"}
+          </span>
           ?
         </Modal.Body>
         <Modal.Footer>
