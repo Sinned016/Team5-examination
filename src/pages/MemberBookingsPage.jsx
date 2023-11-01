f0fc03742ae20561d2e8b147c20b003344d6a6af
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-11-01 12:23:33 +0100

    added modal for avboka button in MemberBookingsPage

--- src/pages/MemberBookingsPage.jsx
+++ src/pages/MemberBookingsPage.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import userService from "../service/userService";
 import fetchOptions from "../service/fetchService";
-
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function MemberBookingsPage() {
   const [bookings, setBookings] = useState([]);
   const email = userService.getUserEmail();
   const navigateToHome = useNavigate();
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchBookings = async () => {
@@ -21,94 +24,129 @@ function MemberBookingsPage() {
     fetchBookings();
   }, [email]); // Dependency on 'email', if it changes, fetch bookings again
 
-  
+  const handleShowModal = (booking) => {
+    setSelectedBooking(booking);
+    setShowModal(true);
+  };
 
-  const cancelBooking = async (bookingId, screeningId) => {
-    
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedBooking(null);
+  };
+
+  const cancelBooking = async () => {
+    setShowModal(false);
+    if (!selectedBooking) return;
     try {
-      const response = await fetch(`/api/bookings/${bookingId}`, fetchOptions('DELETE', { screeningId }));
-      console.log(response);
+      const response = await fetch(
+        `/api/bookings/${selectedBooking._id}`,
+        fetchOptions("DELETE", { screeningId: selectedBooking.screeningId })
+      );
       if (response.ok) {
-        const result = await userService.getUserBookings(email);
-        setBookings(result);
+        const updatedBookings = await userService.getUserBookings(email);
+        setBookings(updatedBookings);
       }
     } catch (err) {
-      console.log(err)
+      console.error("Error cancelling booking:", err);
     }
-  }
+  };
 
   return (
     <>
-    <div className="row mx-1 booking-details-container">
-    <h1>Mina bokningar </h1>
-    <div className="table-responsive mt-4 mb-2">
-    <h2>Bokningsdetaljer</h2>
-    </div>
-    
+      <div className="row mx-1 booking-details-container">
+        <h1>Mina bokningar </h1>
+        <div className="table-responsive mt-4 mb-2">
+          <h2>Bokningsdetaljer</h2>
+        </div>
 
-      {bookings.map((booking, index) => (
-      <table key={index} className="table-dark table-border mb-4">
-    
-          <tbody>
-            <tr>
-              <td className="tdata-left">Bokningsnummer</td>
-              <td className="tdata-right">{booking.bookingNumber}</td>
-            </tr>
-            <tr>
-              <td className="tdata-left">Film</td>
-              <td className="tdata-right">{booking.movieTitle}</td>
-            </tr>
-            <tr>
-              <td className="tdata-left">Datum</td>
-              <td className="tdata-right">
-                {booking.date} {booking.time}
-              </td>
-            </tr>
-            <tr>
-              <td className="tdata-left">Plats</td>
-              <td className="tdata-right">{booking.seats}</td>
-            </tr>
-            <tr>
-              <td className="tdata-left">Pris (betalning sker på plats)</td>
-              <td className="tdata-right">{booking.price} SEK</td>
-            </tr>
-            <div className="d-flex justify-content-center">
-            <button className="btn btn-outline-secondary py-2 mb-4" onClick={() => cancelBooking(booking._id, booking.screeningId)} >AVBOKA</button>
-            </div>
-          </tbody>
-        </table>  ))}
-        
-  
+        {bookings.map((booking, index) => (
+          <table key={index} className="table-dark table-border mb-4">
+            <tbody>
+              <tr>
+                <td className="tdata-left">Bokningsnummer</td>
+                <td className="tdata-right">{booking.bookingNumber}</td>
+              </tr>
+              <tr>
+                <td className="tdata-left">Film</td>
+                <td className="tdata-right">{booking.movieTitle}</td>
+              </tr>
+              <tr>
+                <td className="tdata-left">Datum</td>
+                <td className="tdata-right">
+                  {booking.date} {booking.time}
+                </td>
+              </tr>
+              <tr>
+                <td className="tdata-left">Plats</td>
+                <td className="tdata-right">{booking.seats}</td>
+              </tr>
+              <tr>
+                <td className="tdata-left">Pris (betalning sker på plats)</td>
+                <td className="tdata-right">{booking.price} SEK</td>
+              </tr>
+              <div className="d-flex justify-content-center">
+                <button
+                  className="btn btn-outline-secondary py-2 mb-4"
+                  onClick={() => handleShowModal(booking)}
+                >
+                  AVBOKA
+                </button>
+              </div>
+            </tbody>
+          </table>
+        ))}
 
         <div className="table-responsive mt-4 mb-2">
-    <h2>Bokningshistorik</h2>
-    </div>  
-      <table className="table-dark table-border mb-4">
-        <thead>
-          <th className="tdata-left">Bokningsnr</th>
-          <th className="tdata-left">Film</th>
-          <th className="tdata-left">Datum</th>
-          <th className="tdata-left">Pris</th>
-        </thead>
-              {bookings.map((booking, index) => (
-          <tbody key={index} className="">
+          <h2>Bokningshistorik</h2>
+        </div>
+        <table className="table-dark table-border mb-4">
+          <thead>
+            <th className="tdata-left">Bokningsnr</th>
+            <th className="tdata-left">Film</th>
+            <th className="tdata-left">Datum</th>
+            <th className="tdata-left">Pris</th>
+          </thead>
+          {bookings.map((booking, index) => (
+            <tbody key={index} className="">
+              <tr className="align-bottom">
+                <td className="tdata-left">{booking.bookingNumber}</td>
+                <td className="tdata-left">{booking.movieTitle}</td>
+                <td className="tdata-left">
+                  {booking.date} {booking.time}
+                </td>
 
-            <tr className="align-bottom">
-              <td className="tdata-left">{booking.bookingNumber}</td>
-              <td className="tdata-left">{booking.movieTitle}</td>
-              <td className="tdata-left">
-                {booking.date} {booking.time}
-              </td>
-              
-         
-              <td className="tdata-left">{booking.price} SEK</td>
+                <td className="tdata-left">{booking.price} SEK</td>
               </tr>
-          </tbody> ))}
+            </tbody>
+          ))}
         </table>
-        </div>
-        <div className="d-flex justify-content-center mt-5">
-        <button onClick={() => navigateToHome("/")} className="btn btn-outline-secondary py-2 mb-4 " >STARTSIDA</button>
-        </div>
+      </div>
+      <div className="d-flex justify-content-center mt-5">
+        <button
+          onClick={() => navigateToHome("/")}
+          className="btn btn-outline-secondary py-2 mb-4 "
+        >
+          STARTSIDA
+        </button>
+      </div>
+
+      {/* Modal for cancellation confirmation */}
+      {selectedBooking && (
+        <Modal show={showModal} onHide={handleCloseModal} style={{ color: "black" }}>
+          <Modal.Header closeButton>
+            <Modal.Title>Avbrytbekräftelsen</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Är du säker på att avboka denna bokning?</Modal.Body>
+          <Modal.Footer>
+            <Button className="btn cancel-btn custom-hover-2 me-2" onClick={handleCloseModal}>
+              AVBRYT
+            </Button>
+            <Button className="btn login-btn custom-hover-2" onClick={cancelBooking}>
+              JA
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </>
   );
 }
