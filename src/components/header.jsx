5bbed7e1c1c3f64cb1e8f6719487858dcbecc558
sajo0804 <sandrajonsson86@live.se>
2023-11-03 11:56:09 +0100

    nog looks sort of ok in desktop view. still needs the look for mobile in figma. sandra and roger

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
         {/* <Navbar.Collapse id="basic-navbar-nav"> */}
         <Nav className="nav-top">
           <NavDropdown
-            // className="w-100"
+            // className="dropdown-menu w-100"
             align={{ "git add": "start" }}
             title={
               <svg
@@ -89,15 +89,12 @@ export default function Header() {
               Bokningar {""}{" "}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="16"
+                height="16"
                 fill="currentColor"
-                className="bi bi-list"
+                className="bi bi-ticket"
                 viewBox="0 0 16 16">
-                <path
-                  fillRule="evenodd"
-                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                />
+                <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
               </svg>
             </NavDropdown.Item>
             <NavDropdown.Item href="/" onClick={logout}>
