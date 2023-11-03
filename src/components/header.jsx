318fbcc1047dca4be06c5b2a126ec4c4245f4518
Danblad <rogerdanblad@gmail.com>
2023-11-03 16:10:01 +0100

    Saving changes that works somewhat ok

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 // import { useStates } from "react-easier"
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
@@ -10,13 +10,10 @@ import { NavDropdown } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 
 export default function Header() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const token = localStorage.getItem("JWT_TOKEN");
 
   async function logout() {
     memoryService.clearLocalValue("JWT_TOKEN");
-    userService.removeUserRole();
   }
 
   return (
@@ -31,12 +28,9 @@ export default function Header() {
           />
         </Navbar.Brand>
 
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
         <Nav className="nav-top">
-          <NavDropdown
-            // className="dropdown-menu w-100"
-            align={{ "git add": "start" }}
+        <NavDropdown
+            align={{ "": "start" }}
             title={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -53,6 +47,8 @@ export default function Header() {
               </svg>
             }
             id="basic-nav-dropdown">
+            {!token && (
+              <>
             <NavDropdown.Item href="/register">
               Bli medlem{""}{" "}
               <svg
@@ -85,6 +81,10 @@ export default function Header() {
                 />
               </svg>
             </NavDropdown.Item>
+              </>
+            )}
+            {token && (
+              <>
             <NavDropdown.Item href="/bookings/">
               Bokningar {""}{" "}
               <svg
@@ -117,99 +117,11 @@ export default function Header() {
                 />
               </svg>
             </NavDropdown.Item>
+              </>
+            )}
           </NavDropdown>
         </Nav>
-        {/* </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
 }
-
-// <header>
-//   <nav
-//     id="navbar_top"
-//     class="navbar navbar-expand-lg navbar-dark bg-primary">
-//     <div class="container">
-//       <a class="navbar-brand" href="/">
-//         Brand
-//       </a>
-//       <Dropdown>
-//         <Dropdown.Toggle variant="success" id="dropdown-basic">
-//           Dropdown Button
-//         </Dropdown.Toggle>
-
-//         <Dropdown.Menu>
-//           <Dropdown.Item href="/register">
-//             Bli medlem{""}{" "}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="20"
-//               height="20"
-//               fill="currentColor"
-//               className="bi bi-person-add"
-//               viewBox="0 0 16 16">
-//               <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
-//               <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
-//             </svg>
-//           </Dropdown.Item>
-//           <Dropdown.Item href="/login">
-//             Logga in {""}{" "}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="20"
-//               height="20"
-//               fill="currentColor"
-//               className="bi bi-box-arrow-right"
-//               viewBox="0 0 16 16">
-//               <path
-//                 fillRule="evenodd"
-//                 d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-//               />
-//               <path
-//                 fillRule="evenodd"
-//                 d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-//               />
-//             </svg>
-//           </Dropdown.Item>
-//           <Dropdown.Item href="/bookings/">
-//             Bokningar {""}{" "}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="20"
-//               height="20"
-//               fill="currentColor"
-//               className="bi bi-list"
-//               viewBox="0 0 16 16">
-//               <path
-//                 fillRule="evenodd"
-//                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-//               />
-//             </svg>
-//           </Dropdown.Item>
-//           <Dropdown.Item href="/" onClick={logout}>
-//             Logga ut {""}
-//             {""}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="20"
-//               height="20"
-//               fill="currentColor"
-//               className="bi bi-box-arrow-left"
-//               viewBox="0 0 16 16">
-//               <path
-//                 fillRule="evenodd"
-//                 d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
-//               />
-//               <path
-//                 fillRule="evenodd"
-//                 d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
-//               />
-//             </svg>
-//           </Dropdown.Item>
-//         </Dropdown.Menu>
-//       </Dropdown>
-//     </div>
-//   </nav>
-// </header>
-// );
-// }
