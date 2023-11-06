aebd391ad755f12c93fdda47f242d8b3ea53fb1b
Mikael Forstén <m4jken@gmail.com>
2023-11-06 13:26:09 +0100

    added weekday to screeninglist and fixed formatting to prevent single digit returns on month and day

--- src/service/getFormattedDate.js
+++ src/service/getFormattedDate.js
@@ -1,8 +1,8 @@
 export default function getFormattedDate(date) {
     const today = new Date(date);
-    const month = today.getMonth() + 1;
+    const month = ("0" + (today.getMonth() + 1)).slice(-2); // Adds zero before month and then picks out last two digits. november(11) -> 011 -> 11, janury(1) -> 01 -> 01 (instead of just 1)
     const year = today.getFullYear();
-    const day = today.getDate();
+    const day = ("0" + today.getDate()).slice(-2); // Same as month, to avoid single digit dates.
     const weekday = today.getDay();
     const weekdayArray = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"]
     return `${weekdayArray[weekday]}, ${year}-${month}-${day}`;
