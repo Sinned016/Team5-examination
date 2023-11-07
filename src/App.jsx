c7d123b4aa2c1109367f3ede0393ab2da3946843
Josefine <97985695+josefinelihogberg@users.noreply.github.com>
2023-11-07 11:15:30 +0100

    created AuthContext in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import { createContext, useState, useEffect } from "react";
+import memoryService from "./service/memoryService";
+
+export const AuthContext = createContext({
+  loggedIn: false,
+  logIn: () => {},
+  logOut: () => {},
+});
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setLoggedIn(!!memoryService.getLocalValue("JWT_TOKEN"));
+  }, []);
+
+  const logIn = (token) => {
+    memoryService.saveLocalValue("JWT_TOKEN", token);
+    setLoggedIn(true);
+  };
+
+  const logOut = () => {
+    memoryService.clearLocalValue("JWT_TOKEN");
+    setLoggedIn(false);
+  };
 
   return (
     <div>
-      <header id="header">
-        <Header />
-      </header>
+      <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+        <header id="header">
+          <Header />
+        </header>
 
-      <main>
-        <Container fluid>
-          <Outlet />
-        </Container>
-      </main>
+        <main>
+          <Container fluid>
+            <Outlet />
+          </Container>
+        </main>
 
-      <footer className="container-fixed">
-        <Footer />
-      </footer>
+        <footer className="container-fixed">
+          <Footer />
+        </footer>
+      </AuthContext.Provider>
     </div>
   );
 }
