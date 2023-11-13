9cdb4f6f5f5813641ba9cfcf27db942c89d29876
Dennis Blomberg <97sinned@gmail.com>
2023-11-13 14:33:23 +0100

    Deleted some comments

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,23 +5,6 @@ export default function Footer() {
   return (
     <footer className="footer">
       <Container className="footer-items">
-        {/* <div>
-                    <div className="footer-item"> 
-                        <ul className="list-unstyled mb-0">
-                            <li><a>Om oss</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-item"> 
-                        <ul className="list-unstyled mb-0">
-                            <li><a>Kontakt</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-item"> 
-                        <ul className="list-unstyled mb-0">
-                            <li><a>Hitta hit</a></li>
-                        </ul>
-                    </div>
-                </div> */}
         <p className="footer-item">Om oss</p>
         <p className="footer-item">Kontakt</p>
         <Link to="/location" className="footer-item text-decoration-none text-primary">
