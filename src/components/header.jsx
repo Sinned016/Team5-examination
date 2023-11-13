7a6710f81444aae7237735f898df543cd61605bf
sajo0804 <sandrajonsson86@live.se>
2023-11-13 15:20:56 +0100

    changed from href to link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -45,11 +45,13 @@ export default function Header() {
               <>
                 <NavDropdown.Item className="line-object">
                   <Link
-                    to="/"
+                    to="/register"
                     style={{ textDecoration: "none", color: "#ededed" }}>
-                    <span>Bli medlem</span>
+                    <span>
+                      Bli medlem {""} {""}
+                    </span>
                   </Link>
-                  {""}
+
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
@@ -67,17 +69,19 @@ export default function Header() {
                   <Link
                     to="/login"
                     style={{ textDecoration: "none", color: "#ededed" }}>
-                    Logga in {""}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-ticket"
-                      viewBox="0 0 16 16">
-                      <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
-                    </svg>{" "}
+                    <span>
+                      Logga in {""} {""}
+                    </span>
                   </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-ticket"
+                    viewBox="0 0 16 16">
+                    <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
+                  </svg>{" "}
                 </NavDropdown.Item>
               </>
             ) : (
@@ -86,17 +90,17 @@ export default function Header() {
                   <Link
                     to="/bookings/"
                     style={{ textDecoration: "none", color: "#ededed" }}>
-                    Bokningar {""}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-ticket"
-                      viewBox="0 0 16 16">
-                      <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
-                    </svg>{" "}
+                    Bokningar {""} {""}{" "}
                   </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-ticket"
+                    viewBox="0 0 16 16">
+                    <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
+                  </svg>{" "}
                 </NavDropdown.Item>
                 <div className="dropdown-divider"></div>
 
@@ -106,7 +110,7 @@ export default function Header() {
                     style={{ textDecoration: "none", color: "#ededed" }}>
                     <span>Logga ut</span>
                   </Link>
-                  {""}
+                  {""} {""}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
