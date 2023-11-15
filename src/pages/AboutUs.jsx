3c41ed39a18491b8a1d1f1773220e0ec8661ca04
sajo0804 <sandrajonsson86@live.se>
2023-11-15 10:59:46 +0100

    styling on aboutUs-page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,34 +1,34 @@
 import { useNavigate } from "react-router-dom";
-
+import Container from "react-bootstrap/Container";
 
 export default function AboutUsPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="about-us-page d-flex row m-2">
-            <div className="">
-                <h1>Om oss</h1>
-                <p>Välkommen till oss på Filmvisarnas bio!
-                    <br/>
-                    Vi är en liten bio med två salonger där vi visar olika filmer, allt från gamla godingar till nya storfilmer.
-                </p>
-            </div>
-
-            <div>
-                <h3>Filmvisarnas shop</h3>
-                <p>Vi har försäljning av popcorn, chips, godis och dricka .</p>
-                <div className="candy-shop border">                    
-                    {/* shop images */}
-                    <p> Bilder på vad vi säljer kommer</p>
-                </div>
-            </div>
-            <div className="mt-3">
-                    <p>Se vad vi visar på bio just nu
-                        <br/>
-                        <button onClick={() => navigate ("/")} className="about-us-btn">Tillbaka till filmer</button>
-                    </p>
-                    <h2>Varmt välkomna till oss</h2>
-                </div>
+  return (
+    <div className="aboutUs d-flex justify-content-center">
+      <div className="about m-3 ">
+        <div className="aboutUs">
+          <h1>Om oss</h1>
+          <p>
+            Välkommen till oss på Filmvisarnas biograf!
+            <br />
+            Vi är en liten biograf med två salonger där vi visar olika filmer,
+            <br />
+            allt från gamla godingar till nya storfilmer.
+          </p>
         </div>
-    )
+
+        <div className="shop">
+          <h3>Filmvisarnas shop</h3>
+          <p>Vi har försäljning av popcorn, chips, godis och dricka .</p>
+        </div>
+        <div className="mt-3">
+          <br />
+          <button onClick={() => navigate("/")} className="about-us-btn">
+            Tillbaka till filmer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
