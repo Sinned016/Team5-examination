5f24d3752f8f9c17bd54534f83ce3b73ed67bad9
Mikael Forstén <m4jken@gmail.com>
2023-11-15 11:57:18 +0100

    setup col configuration

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,51 +1,51 @@
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FormControl, FormGroup, FormLabel, FormText } from "react-bootstrap";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col, Container } from "react-bootstrap";
 
 export default function ContactPage() {
   return (
-    <>
-      <div className="contactPageText  d-flex justify-content-center">
-        <div className="contact-page d-flex row m-2">
-          <div className="contact-text">
-            <h1>Kontakta oss</h1>
-            <p>
-              Har du några frågor om din bokning?
+    <div className="container-xl">
+      <div className="row">
+        <div className="col-md-6 col-lg-6 col-xl-7 col-xxl-8">
+            <div className="contact-text m-2">
+              <h1>Kontakta oss</h1>
+              <p>
+                Har du några frågor om din bokning?
+                <br />
+                Eller har du andra funderingar om Filmvisarnas biograf?
+              </p>
+              <p />
+              Ring oss på telefonnummer:
               <br />
-              Eller har du andra funderingar om Filmvisarnas biograf?
-            </p>
-            <p />
-            Ring oss på telefonnummer:
-            <br />
-            <FontAwesomeIcon icon={faPhone} /> 020-123456
-            <p>eller skicka din fråga via formuläret nedan</p>
-          </div>
+              <FontAwesomeIcon icon={faPhone} /> 020-123456
+              <p>eller skicka din fråga via formuläret nedan</p>
+            </div>
         </div>
-        <div className="ContactPageForm ">
-          <Form className="contact-form d-flex row mt-2 justify-content-center">
-            <FormGroup className="mb-3" controlId="formName">
-              <FormLabel>Namn</FormLabel>
-              <FormControl placeholder="Skriv ditt namn här" />
-            </FormGroup>
-            <FormGroup className="mb-3" controlId="formEmail">
-              <FormLabel>Email</FormLabel>
-              <FormControl type="email" placeholder="namn@exempel.com" />
-            </FormGroup>
-            <FormGroup className="mb-3" controlId="formTextField">
-              <FormLabel>Meddelande</FormLabel>
-              <FormControl
-                as="textarea"
-                rows={3}
-                placeholder="Ställ din fråga här"
-              />
-            </FormGroup>
-            <Button className="send-form-btn mb-4" type="submit">
-              Skicka
-            </Button>
-          </Form>
+        <div className="col-md-6 col-lg-6 col-xl-5 col-xxl-4">
+            <Form className="contact-form d-flex row mt-2 justify-content-center">
+              <FormGroup className="mb-3" controlId="formName">
+                <FormLabel>Namn</FormLabel>
+                <FormControl placeholder="Skriv ditt namn här" />
+              </FormGroup>
+              <FormGroup className="mb-3" controlId="formEmail">
+                <FormLabel>Email</FormLabel>
+                <FormControl type="email" placeholder="namn@exempel.com" />
+              </FormGroup>
+              <FormGroup className="mb-3" controlId="formTextField">
+                <FormLabel>Meddelande</FormLabel>
+                <FormControl
+                  as="textarea"
+                  rows={3}
+                  placeholder="Ställ din fråga här"
+                />
+              </FormGroup>
+              <Button className="send-form-btn mb-4" type="submit">
+                Skicka
+              </Button>
+            </Form>
         </div>
       </div>
-    </>
+    </div>          
   );
 }
