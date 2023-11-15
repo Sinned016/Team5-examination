b52142a76c52f657cfcdc3bf558026bf36521791
sajo0804 <sandrajonsson86@live.se>
2023-11-15 11:28:48 +0100

    styling on contactPage. Desktop and tablet done

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,44 +5,47 @@ import { Form, Button } from "react-bootstrap";
 
 export default function ContactPage() {
   return (
-    <div className="contactPage  d-flex justify-content-center">
-      <div className="contact-page d-flex row m-2">
-        <div className="contact-text">
-          <h1>Kontakta oss</h1>
-          <p>
-            Har du några frågor om din bokning?
+    <>
+      <div className="contactPageText  d-flex justify-content-center">
+        <div className="contact-page d-flex row m-2">
+          <div className="contact-text">
+            <h1>Kontakta oss</h1>
+            <p>
+              Har du några frågor om din bokning?
+              <br />
+              Eller har du andra funderingar om Filmvisarnas biograf?
+            </p>
+            <p />
+            Ring oss på telefonnummer:
             <br />
-            Eller har du andra funderingar om Filmvisarnas biograf?
-          </p>
-          <p />
-          Ring oss på telefonnummer:
-          <br />
-          <FontAwesomeIcon icon={faPhone} /> 020-123456
-          <p>eller skicka din fråga via formuläret nedan</p>
+            <FontAwesomeIcon icon={faPhone} /> 020-123456
+            <p>eller skicka din fråga via formuläret nedan</p>
+          </div>
+        </div>
+        <div className="ContactPageForm ">
+          <Form className="contact-form d-flex row mt-2 justify-content-center">
+            <FormGroup className="mb-3" controlId="formName">
+              <FormLabel>Namn</FormLabel>
+              <FormControl placeholder="Skriv ditt namn här" />
+            </FormGroup>
+            <FormGroup className="mb-3" controlId="formEmail">
+              <FormLabel>Email</FormLabel>
+              <FormControl type="email" placeholder="namn@exempel.com" />
+            </FormGroup>
+            <FormGroup className="mb-3" controlId="formTextField">
+              <FormLabel>Meddelande</FormLabel>
+              <FormControl
+                as="textarea"
+                rows={3}
+                placeholder="Ställ din fråga här"
+              />
+            </FormGroup>
+            <Button className="send-form-btn mb-4" type="submit">
+              Skicka
+            </Button>
+          </Form>
         </div>
       </div>
-
-      <Form className="contact-form d-flex row mt-2 justify-content-center">
-        <FormGroup className="mb-3" controlId="formName">
-          <FormLabel>Namn</FormLabel>
-          <FormControl placeholder="Skriv ditt namn här" />
-        </FormGroup>
-        <FormGroup className="mb-3" controlId="formEmail">
-          <FormLabel>Email</FormLabel>
-          <FormControl type="email" placeholder="namn@exempel.com" />
-        </FormGroup>
-        <FormGroup className="mb-3" controlId="formTextField">
-          <FormLabel>Meddelande</FormLabel>
-          <FormControl
-            as="textarea"
-            rows={3}
-            placeholder="Ställ din fråga här"
-          />
-        </FormGroup>
-        <Button className="send-form-btn mb-4" type="submit">
-          Skicka
-        </Button>
-      </Form>
-    </div>
+    </>
   );
 }
